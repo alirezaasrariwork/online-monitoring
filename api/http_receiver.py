95d3e06fe8b3901cd93b48ccbfc740b7ef620696
alirezaasrari <alirezaasrariwork@gmail.com>
2026-02-02 15:21:12 +0330

    change logic module

--- api/http_receiver.py
+++ api/http_receiver.py
@@ -7,30 +7,10 @@ from fastapi.responses import JSONResponse
 import time
 import logging
 import os
-
+from alarm_logic import logic_a,logic_b,logic_c
 # -------------------------------
 # Import your logic functions
 # -------------------------------
-def logic_a(value):
-    if value > 80:
-        return "CRITICAL", 1, "Cool down system"
-    elif value > 60:
-        return "WARN", 1, "Reduce load"
-    return "OK", 0, "Normal"
-
-def logic_b(value):
-    if value > 120:
-        return "CRITICAL", 1, "Release pressure"
-    elif value > 90:
-        return "WARN", 1, "Monitor pressure"
-    return "OK", 0, "Normal"
-
-def logic_c(value):
-    if value > 7:
-        return "CRITICAL", 1, "Stop machine"
-    elif value > 4:
-        return "WARN", 1, "Schedule maintenance"
-    return "OK", 0, "Normal"
 
 # -------------------------------
 # InfluxDB config
