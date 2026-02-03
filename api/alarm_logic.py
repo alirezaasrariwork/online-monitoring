9432ed4b590dafbdb5cb7f980044534c6a769404
alirezaasrari <alirezaasrariwork@gmail.com>
2026-02-03 09:55:02 +0330

    bug fixed in check alarm

--- api/alarm_logic.py
+++ api/alarm_logic.py
@@ -1,20 +1,20 @@
 def logic_a(value):
     if value > 80:
-        return "CRITICAL", True, "Cool down system"
+        return "CRITICAL", 1, "Cool down system"
     elif value > 60:
-        return "WARN", True, "Reduce load"
-    return "OK", False, "Normal"
+        return "WARN", 1, "Reduce load"
+    return "OK", 0, "Normal"
 
 def logic_b(value):
     if value > 120:
-        return "CRITICAL", True, "Release pressure"
+        return "CRITICAL", 1, "Release pressure"
     elif value > 90:
-        return "WARN", True, "Monitor pressure"
-    return "OK", False, "Normal"
+        return "WARN", 1, "Monitor pressure"
+    return "OK", 0, "Normal"
 
 def logic_c(value):
     if value > 7:
-        return "CRITICAL", True, "Stop machine"
+        return "CRITICAL", 1, "Stop machine"
     elif value > 4:
-        return "WARN", True, "Schedule maintenance"
-    return "OK", False, "Normal"
+        return "WARN", 1, "Schedule maintenance"
+    return "OK", 0, "Normal"
